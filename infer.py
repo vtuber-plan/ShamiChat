2e8121f03f6cec4d46177bf130870ce903785acc
jstzwj <1103870790@qq.com>
2023-03-29 00:14:41 +0800

    update

--- infer.py
+++ infer.py
@@ -38,11 +38,11 @@ model = model.to(device)
 
 hparams = model.hparams
 
-inputs = tokenizer(["请问答社区上"], return_tensors="pt").to(device)
+inputs = tokenizer(["唐代杜甫"], return_tensors="pt").to(device)
 input_ids = inputs["input_ids"]
 
 
-outputs = model.net.generate(**inputs, penalty_alpha=0.6, top_k=4, max_new_tokens=100)
+outputs = model.net.generate(**inputs, penalty_alpha=0.3, top_k=50, max_new_tokens=100)
 # outputs = model.net.generate(**inputs, num_beams=5, max_new_tokens=50)
 out_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 print(out_text)
